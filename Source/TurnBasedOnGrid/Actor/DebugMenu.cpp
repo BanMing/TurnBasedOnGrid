2d11121d0188a13baca560c29c12faeda3cac44f
BanMing <ban-ming@foxmail.com>
2025-09-07 20:05:18 +0800

    add debug menu widget

---
+++ Source/TurnBasedOnGrid/Actor/DebugMenu.cpp
@@ -0,0 +1,28 @@
+// Copyright BanMing
+
+
+#include "Actor/DebugMenu.h"
+
+
+// Sets default values
+ADebugMenu::ADebugMenu()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADebugMenu::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADebugMenu::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/TurnBasedOnGrid/Actor/DebugMenu.h
@@ -0,0 +1,28 @@
+// Copyright BanMing
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DebugMenu.generated.h"
+
+UCLASS()
+class TURNBASEDONGRID_API ADebugMenu : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADebugMenu();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
